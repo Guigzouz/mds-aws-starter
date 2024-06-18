8cf30995a4d74851843bb2d0952d132bf3b4be93
Guigzouz <benard.guillaume0@gmail.com>
2024-06-18 11:18:34 +0200

    oupsi asynchrone teehee ^^

--- index.test.js
+++ index.test.js
@@ -1,10 +1,21 @@
-// tests/index.test.js
 const request = require("supertest");
 const app = require("./index");
+const http = require("http");
+
+let server;
+
+beforeAll((done) => {
+  server = http.createServer(app);
+  server.listen(done);
+});
+
+afterAll((done) => {
+  server.close(done);
+});
 
 describe("GET /", () => {
   it("should respond with Hello, World!", async () => {
-    const response = await request(app).get("/");
+    const response = await request(server).get("/");
     expect(response.status).toBe(200);
     expect(response.text).toBe("Hello, World!");
   });
